8009397d7420a2b68d7d2f07494078b2dd7412f0
Alfred Mtambalika <karanlicka@gmail.com>
2026-05-05 21:56:22 +0530

    feat: Create in-memory todo store

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const app = express();
 
 app.use(express.json());
 
+const todos = [
+  { id: 1, title: 'Learn Express', completed: false },
+  { id: 2, title: 'Build API', completed: false }
+];
+
 app.get('/', (req, res) => {
   res.send('API is running');
 });
@@ -12,4 +17,5 @@ const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  console.log('Todos:', todos);
 });
